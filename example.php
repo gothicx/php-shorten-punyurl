36377ec3b239f3b4b942461a775e32de992e1a86
Marco Rodrigues <gothicx@sapo.pt>
2009-07-21 22:16:45 +0100

    first commit

---
+++ example.php
@@ -0,0 +1,28 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<title>SAPO PunyURL shortening service examples</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+</head>
+
+<body>
+
+<?php
+
+require_once("punyurl.class.php");
+
+$puny=new PunyURL;
+
+$shorten=$puny->short("http://marco.tondela.org");
+$original=$puny->long("http://瀟.sl.pt");
+
+echo 'Puny URL: '.$shorten['puny']."<br>";
+echo 'ASCii URL: '.$shorten['ascii']."<br>";
+echo 'Preview URL: '.$shorten['preview']."<br><br>";
+echo 'Original URL: '.$original['url'];
+
+?>
+
+</body>
+</html>
---
+++ punyurl.class.php
@@ -0,0 +1,67 @@
+<?php
+
+/*
+	PunyURL is a URL shortening service provided by SAPO (http:/www.sapo.pt)
+	Homepage: http://services.sapo.pt/Metadata/Service/PunyURL?culture=EN
+	
+	Dependencies to run this class:
+	- PHP: version 5.x
+	- Package: php5-curl.
+
+	Copyright (C) 2009 Marco Rodrigues <http://Marco.Tondela.org>
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License along
+	with this program; if not, write to the Free Software Foundation, Inc.,
+	51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+class PunyURL {
+	function get_data($url="") {
+		$csess=curl_init();
+		curl_setopt($csess,CURLOPT_URL,$url);
+		curl_setopt($csess,CURLOPT_RETURNTRANSFER,1);
+		// Timeout when connecting to the service (5 seconds).
+		curl_setopt($csess,CURLOPT_CONNECTTIMEOUT,5);
+		$data=curl_exec($csess);
+		curl_close($csess);
+
+		return $data;
+	}
+
+	// Convert long URL into two different types of shortened URL's.
+	function short($url="") {
+		// The URL for PunyURL service at SAPO.
+		$surl="http://services.sapo.pt/PunyURL/GetCompressedURLByURL?url=".urlencode($url);
+
+		// Get the information from XML response.
+		$xml=simplexml_load_string($this->get_data($surl));
+		$shorten['puny']=$xml->puny;
+		$shorten['ascii']=$xml->ascii;
+		$shorten['preview']=$xml->preview;
+
+		return $shorten;
+	}
+
+	// Given a short URL that you got previously from short() function, return the Original URL.
+	function long($url="") {
+		// The URL for PunyURL service at SAPO.
+		$lurl="http://services.sapo.pt/PunyURL/GetURLByCompressedURL?url=".urlencode($url);
+
+		// Get the information from XML response.
+		$xml=simplexml_load_string($this->get_data($lurl));
+		$original['url']=$xml->url;
+
+		return $original;
+	}
+}
+
+?>
